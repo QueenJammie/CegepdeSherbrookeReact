287446725546af84d019c2332bef0ef9463ead90
QueenJammie <jammie.lacc@outlook.com>
2022-07-23 15:09:27 -0400

    Added content for every courses and fixed the design

--- src/Footer.js
+++ src/Footer.js
@@ -1,16 +1,16 @@
 import React from "react";
 import logo from './logo.svg';
 
-import "./App.css";
+import "./Footer.css";
 
 export default function Footer()
 {
     return (
         <footer className="App-footer">
             <hr />
-            <div className="col-11">
+            <div className="col-12 footer">
                 <img src={logo} className="App-logo" alt="logo" />
-                <a href="https://github.com/QueenJammie/CegepdeSherbrookeReact/tree/main/src" className="openSourceCode-link">OpenSource Code</a> by <a href="mailto: jammie.lacc@outlook.com" className="student-link">Student Jammie Lachance-Côté</a>
+                <a href="https://github.com/QueenJammie/CegepdeSherbrookeReact/tree/main/src" className="openSourceCode-link">OpenSource Code</a>   by  <a href="mailto: jammie.lacc@outlook.com" className="student-link">Jammie Lachance-Côté</a>
             </div> 
         </footer>
     )
--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
 import "./Main.css";
 
@@ -12,6 +12,126 @@ export default function Main()
     event.preventDefault();
     alert(`Searching for ${course}`);
     if (course === "420-PA1-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PA2-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PA3-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PA4-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PB1-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PB2-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PB3-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PB4-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PC1-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PC2-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PC3-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PC4-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PD1-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PD2-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PD3-SH")
+    {
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
+        </div>
+      )
+    }
+    else if (course === "420-PES-SH")
     {
       setDisplay(
         <div className="display">
@@ -38,9 +158,11 @@ export default function Main()
     <div className="Main">
       <form onSubmit={handleSubmit}>
       <input type="search" className="form-control" onChange={updateCourse} placeholder="420-Pxx-SH" autofocus="on" />
-      <input type="submit" value="Search for notes" className="mt-2 btn-primary" />
+      <input type="submit" value="Search for notes" className="mt-2 btn btn-success" />
       </form>
-      {display}
+      <div className="display">
+        {display}
+      </div>
     </div>
   )
 }
