839dbd5049b7cf3a6eef8bf67df4d920db3f9c0f
QueenJammie <jammie.lacc@outlook.com>
2022-07-22 22:13:05 -0400

    Added a notes space displayed after a valid search in the input

--- src/Main.js
+++ src/Main.js
@@ -4,25 +4,43 @@ import "./Main.css";
 
 export default function Main()
 {
-    const [course, setCourse] = useState(null);
+  let [course, setCourse] = useState(null);
+  let [display, setDisplay] = useState(null);
 
-    function handleSubmit(event)
+  function handleSubmit(event)
+  {
+    event.preventDefault();
+    alert(`Searching for ${course}`);
+    if (course === "420-PA1-SH")
     {
-        event.preventDefault();
-        alert(`Searching for ${course}`);
-    }
-
-    function updateCourse(event)
-    {
-        setCourse(event.target.value);
-    }
-
-    return (
-        <div className="Main">
-            <form onSubmit={handleSubmit}>
-            <input type="search" className="form-control" onChange={updateCourse} />
-            <input type="submit" value="Search" className="mt-2 btn-primary" />
-            </form>
+      setDisplay(
+        <div className="display">
+          <p>Notes pour {course}: </p>
         </div>
-    )
+      )
+    }
+    else
+    {
+      setDisplay(
+        <div className="display">
+          <p>Entrez le nom d'une formation dans le formulaire</p>
+        </div>
+      )
+    }
+  }
+
+  function updateCourse(event)
+  {
+    setCourse(event.target.value);
+  }
+
+  return (
+    <div className="Main">
+      <form onSubmit={handleSubmit}>
+      <input type="search" className="form-control" onChange={updateCourse} placeholder="420-Pxx-SH" autofocus="on" />
+      <input type="submit" value="Search for notes" className="mt-2 btn-primary" />
+      </form>
+      {display}
+    </div>
+  )
 }
